57e6ba3f3f68736d3eab9b70bbb43a28b7c825d5
文思为 <wensiwei@qianxin.com>
2026-04-08 08:58:53 +0800

    add

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use std::env;
+
 use wp_station::server::start;
+
+const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[tokio::main]
 async fn main() {
+    let show_version = env::args().any(|arg| arg == "--version" || arg == "-V");
+
+    if show_version {
+        println!("wp-station {}", APP_VERSION);
+        return;
+    }
+
     start().await.expect("启动服务器失败");
 }
